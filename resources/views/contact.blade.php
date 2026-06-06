c6a6a0a778be99bb1238d4ab6fc7bcdabac78c06
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-06-06 20:35:17 +0545

    routing and redirection

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact me!</title>
+</head>
+<body>
+    <h1>Contact Me</h1>
+    <h2>React out to me if you want 1 to 1 coding classes with me!</h2>
+    <h3>React out to me if you want your kid 1 to 1 coding classes with me!</h3>
+    <p>Feel free to reach out!</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+// Route::get("/about", function() {
+//     return view('about');
+// });
+
+Route::get("/about/{name}", function($name) {
+    return view('about', ['name' => $name]);
+});
+
+Route::redirect('/about', '/about/John');
